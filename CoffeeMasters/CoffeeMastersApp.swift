b655f0e399f13412626dc7520951d8bb1a537c65
JoaoCardoso00 <jvcq13@gmail.com>
2023-09-05 19:48:09 -0300

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by joao cardoso on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
